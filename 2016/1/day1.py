ad783e5cc464c42547ad39f48524831f0fd90d1c
Benjamin Orthner <benjaminorthner@gmail.com>
2021-12-07 13:20:42 +0100

    removed comment

--- 2016/1/day1.py
+++ 2016/1/day1.py
@@ -2,62 +2,8 @@ import numpy as np
 
 with open("input.dat", "r") as file:
     lines = [line.strip() for line in file.readlines()]
-
 positions = lines[0].split(",")
 
-'''
-x = 0
-y = 0
-caxis = "x"
-cdir = 1
-for p in positions:
-    p = p.strip()
-    dir = p[0]
-    step = int(p[1:])
-    
-    if caxis == "x" and dir == "R" and cdir == 1:
-        caxis = "y"
-        cdir = 1
-        y += step*cdir
-    
-    if caxis == "x" and dir == "L" and cdir == 1:
-        caxis = "y"
-        cdir = -1
-        y += step*cdir
-
-    if caxis == "x" and dir == "R" and cdir == -1:
-        caxis = "y"
-        cdir = -1
-        y += step*cdir
-    
-    if caxis == "x" and dir == "L" and cdir == -1:
-        caxis = "y"
-        cdir = 1
-        y += step*cdir
-
-    if caxis == "y" and dir == "R" and cdir == 1:
-        caxis = "x"
-        cdir = -1
-        x += step*cdir
-
-    if caxis == "y" and dir =="L" and cdir == 1:
-        caxis = "x"
-        cdir = 1
-        x += step*cdir
-
-    if caxis == "y" and dir == "R" and cdir == -1:
-        caxis = "x"
-        cdir = 1
-        x += step*cdir
-
-    if caxis == "y" and dir =="L" and cdir == -1:
-        caxis = "x"
-        cdir = -1
-        x += step*cdir
-
-
-print(x + y)
-'''
 
 x, y = 0, 0
 compass = ["N", "W", "S", "E"]
@@ -79,7 +25,7 @@ for p in positions:
     if dir == "E": x += step
     if dir == "W": x -= step
 
-#print(x + y)
+print(x + y)
 
 
 # part 2
