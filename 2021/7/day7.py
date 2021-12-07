1508ff1a8a0b80be189cfe96a7cda90f54ffe67e
Benjamin Orthner <benjaminorthner@gmail.com>
2021-12-07 07:27:18 +0100

    day7

--- 2021/7/day7.py
+++ 2021/7/day7.py
@@ -4,3 +4,13 @@ import math
 
 with open("input.dat", "r") as file:
     lines = [line.strip() for line in file.readlines()]
+positions = list(map(int,lines[0].split(",")))
+
+# part 1
+spendlist = [sum([np.abs(p - targetP) for p in positions]) for targetP in range(max(positions))]
+print(min(spendlist))
+
+# part 2
+sumallbelow = lambda x: int((x*(x+1)) / 2)
+spendlist = [sum([sumallbelow(np.abs(p - targetP)) for p in positions]) for targetP in range(max(positions))]
+print(min(spendlist))
