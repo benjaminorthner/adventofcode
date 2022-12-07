2544459fccdf1cd181f02ad04bb3b8f80b8bbb17
Benjamin Orthner <benjaminorthner@gmail.com>
2022-12-07 16:38:05 +0100

    day7 complete

--- 2022/07/day7.py
+++ 2022/07/day7.py
@@ -7,8 +7,20 @@ class Node(object):
     def __init__(self, nodeName=None, parentNode=None) -> None:
         self.nodeName = nodeName
         self.parentNode = parentNode
-        self.subNodes = None
-        self.content = None
+        self.subNodes = []
+        self.content = []
+    
+    def get_size(self):
+        self.size = 0
+        for item in self.content:
+            self.size += item['size']
+        
+        # add size of subNodes
+        for subNode in self.subNodes:
+            self.size += subNode.get_size()
+        
+        return self.size
+
 
 # ------------------------------------------------------------
 # import input and put queries and responses into nicer format
@@ -35,6 +47,9 @@ for line in lines:
 
     # if not new query then must be response
     response.append(line.split(" "))
+commands.append({'query': query, 'response':deepcopy(response)})
+
+
 
 # ---------------------------------------
 # Build the tree structure from the input
@@ -42,11 +57,11 @@ for line in lines:
 
 # define root node
 root = Node("/")
+list_of_folders = [root]
 current_folder = root
 
 # loop over all commands
 for command in commands[1:]:
-
     # if query is "ls"
     if command['query'][0] == 'ls':
         sub_folders = []
@@ -54,11 +69,13 @@ for command in commands[1:]:
         for response in command['response']:
             # if dir, then definitely a new directory (input does not show same ls response twice)
             if response[0] == 'dir':
-                sub_folders.append(Node(nodeName=response[1], parentNode=current_folder))
+                new_folder = Node(nodeName=response[1], parentNode=current_folder)
+                sub_folders.append(new_folder)
+                list_of_folders.append(new_folder)
                 continue
 
             # else just files. Save in dict with name and size
-            files.append({'filename':response[1], 'size':response[0]})
+            files.append({'filename':response[1], 'size':int(response[0])})
 
         # save subfolders and files into node
         current_folder.subNodes = sub_folders
@@ -75,3 +92,28 @@ for command in commands[1:]:
     for sub_folder in current_folder.subNodes:
         if sub_folder.nodeName == command['query'][1]:
             current_folder = sub_folder
+
+# ------
+# PART 1
+# ------
+part1 = 0
+for f in list_of_folders:
+    size = f.get_size()
+    if size <= 100000:
+        part1 += size
+
+print(part1)
+
+# ------
+# PART 2
+# ------
+total_space = 70000000
+needed_space = 30000000
+occupied_space = root.get_size()
+
+list_of_possible_delete_sizes = []
+for f in list_of_folders:
+    if total_space - occupied_space + f.get_size() >= needed_space:
+        list_of_possible_delete_sizes.append(f.get_size())
+
+print(sorted(list_of_possible_delete_sizes)[0])
