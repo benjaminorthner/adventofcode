1c492fb8aef8538104fb19c20c137871424b8fd0
Benjamin Orthner <benjaminorthner@gmail.com>
2022-12-18 21:12:57 +0100

    day18

--- 2022/18/day18.py
+++ 2022/18/day18.py
@@ -1,19 +1,4 @@
-from mpl_toolkits.mplot3d import Axes3D
-import numpy as np
-import matplotlib.pyplot as plt
-N1 = 10
-N2 = 10
-N3 = 10
-ma = np.random.choice([0,1], size=(N1,N2,N3), p=[0.99, 0.01])
-
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-ax.set_aspect('equal')
-
-ax.voxels(ma, edgecolor="k")
-
-plt.show()
-
+# I SHOULD SOLVE PART2 BY FLOOD FILLING THE OUTSIDE REGION INSTEAD OF THE WAY I DO IT NOW
 
 with open('input.dat', 'r') as file:
     lines = [line.strip() for line in file.readlines()]
